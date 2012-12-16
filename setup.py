7eb7d2e0e268d072b8b93b82b0f4ac86a1737439
TsungWei Hu <marr.tw@gmail.com>
2012-12-16 08:46:32 +0800

    tbfac skins files added. modernizr-2.6.1-respond-1.1.0.min.js renamed to modernizr.min.js

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(name='tbfac.theme',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
+#     setup_requires=["PasteScript"],
+#     paster_plugins=["ZopeSkel"],
       )
