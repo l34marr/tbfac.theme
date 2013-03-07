a8e233773a2dca6e04dcc774a7568cc7857652b9
TsungWei Hu <marr.tw@gmail.com>
2013-03-07 23:51:50 +0800

    Update Homepage Browser View.

--- tbfac/theme/browser/homepage.py
+++ tbfac/theme/browser/homepage.py
@@ -1,5 +1,8 @@
 from zope.interface import implements
+from zope.component import getMultiAdapter
+from Acquisition import aq_inner
 
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 
 from tbfac.theme.browser.interfaces import IHomepage
@@ -10,11 +13,47 @@ class Homepage(BrowserView):
     implements(IHomepage)
     
     def fixed_items(self):
-        pass
+        """ Get Items for Fixed-Position Tiles
+        """
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        portal_state = getMultiAdapter((context, self.request),
+            name=u'plone_portal_state')
+        path = portal_state.navigation_root_path() + '/fixed'
+        return catalog(portal_type='tbfac.Advert',
+                       review_state='published',
+                       path=path,
+                       sort_on='getObjPositionInParent',
+                       sort_order='ascending',
+                       sort_limit=9)[:9]
 
     def jury_items(self):
-        pass
+        """ Get Items for Jury Tiles
+        """
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        portal_state = getMultiAdapter((context, self.request),
+            name=u'plone_portal_state')
+        path = portal_state.navigation_root_path() + '/juries/2013'
+        return catalog(portal_type='tbfac.Jury',
+                       review_state='published',
+                       path=path,
+                       sort_on='getObjPositionInParent',
+                       sort_order='ascending',
+                       sort_limit=7)[:7]
 
     def info_items(self):
-        pass
+        """ Get Items for Info Tiles
+        """
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        portal_state = getMultiAdapter((context, self.request),
+            name=u'plone_portal_state')
+        path = portal_state.navigation_root_path() + '/event/info'
+        return catalog(portal_type='tbfac.Info',
+                       review_state='published',
+                       path=path,
+                       sort_on='created',
+                       sort_order='descending',
+                       sort_limit=12)[:12]
 
